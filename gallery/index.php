8e017b776b7db34cda333ee6bda01f9f941f5b68
Mohammad Adrian <126534278+Muhyann@users.noreply.github.com>
2024-02-17 11:25:03 +0700

    Add files via upload

---
+++ gallery/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>index</title>
+</head>
+<body>
+    <h1></h1>
+    <?php
+        session_start();
+        if(!isset($_SESSION['userid'])){
+    ?>
+            <ul>
+                <li><a href="register.php">Register</a></li>
+                <li><a href="login.php">Login</a></li>
+            </ul>
+    <?php
+        }else{
+    ?>   
+        <p>Selamat datang <b><?=$_SESSION['namalengkap']?></b></p>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="album.php">Album</a></li>
+            <li><a href="foto.php">Foto</a></li>
+            <li><a href="logout.php">Logout</a></li>
+        </ul>
+    <?php
+        }
+    ?>
+</body>
+</html>
